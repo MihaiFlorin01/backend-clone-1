ce36919dac2179a2621a1a3a2d8f048f102d6cf0
Florin <mihaiflorin01194@yahoo.com>
2021-04-13 16:23:05 +0300

    backend-clone-1

--- src/main/java/com/example/backendclone1/schedule/StatusScheduler.java
+++ src/main/java/com/example/backendclone1/schedule/StatusScheduler.java
@@ -1,11 +1,8 @@
 package com.example.backendclone1.schedule;
 
 import com.example.backendclone1.controller.CloneController;
-import com.example.backendclone1.model.Clone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
